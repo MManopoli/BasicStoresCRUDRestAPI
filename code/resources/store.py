c95b6d9c0a94ae7813b3e40397d9054661392b6d
mmanopoli <matthew.manopoli@gmail.com>
2020-01-17 21:00:56 +0000

    adding jwt wrapper calls to remaining functions - although not used.

--- code/resources/store.py
+++ code/resources/store.py
@@ -1,14 +1,17 @@
 from flask_restful import Resource
 from models.store import StoreModel
+from flask_jwt import jwt_required
 
 
 class Store(Resource):
+    # @jwt_required()
     def get(self, name):
         store = StoreModel.find_by_name(name)
         if store:
             return store.json()
         return {'message': 'Store not found'}, 404
 
+    # @jwt_required()
     def post(self, name):
         if StoreModel.find_by_name(name):
             return {'message': f'A store with name {name} already exists.'}, 400
@@ -21,6 +24,7 @@ class Store(Resource):
 
         return store.json(), 201
 
+    # @jwt_required()
     def delete(self, name):
         store = StoreModel.find_by_name(name)
         if store:
@@ -30,6 +34,7 @@ class Store(Resource):
 
 
 class StoreList(Resource):
+    @jwt_required()
     def get(self):
         return {'stores': [store.json() for store in StoreModel.query.all()]}
 
